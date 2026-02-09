e71ecb1e86dd9ec54fe95464bae7d272994707d9
VoidOfMaya <mayaofthevoid@gmail.com>
2026-02-09 09:36:39 +0200

    feat(auth)parses data for user registry

--- Auth/authController.js
+++ Auth/authController.js
@@ -1,4 +1,5 @@
 import { validationResult, matchedData } from "express-validator";
+import bcrypt from 'bcryptjs';
 
 async function createUser(req, res){
     const errors = validationResult(req);
@@ -7,7 +8,22 @@ async function createUser(req, res){
         return res.status(400).json({errors: errors.array()});
     }
     const data = matchedData(req);
-    console.log(data)
+        try{
+            const parsedData = {
+                firstName: data.firstName,
+                lastName: data.lastName, 
+                username: data.username, 
+                password: await bcrypt.hash(data.password, 10),
+            };
+            console.log(parsedData);
+            //await postgres.addMember(secureData);
+            //console.log(`Registration success`);
+    }catch(err){
+        console.log(err)
+        res.redirect('/')
+    }
+    console.log(data);
+    res.redirect('/')
 }
 
 export{
