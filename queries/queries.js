62f3d761877e2c1888f710d001cb68a7b807ef4a
VoidOfMaya <mayaofthevoid@gmail.com>
2026-02-10 09:33:02 +0200

    docs(prisma/queries)documents basic required prisma queries

--- queries/queries.js
+++ queries/queries.js
@@ -1,1 +1,16 @@
 import {prisma} from '../lib/prisma';
+
+
+/* required queries
+user based:
+1.get user if exists
+2.create new user
+
+
+file  based:
+
+C-  store new file in the db
+R-  read existing file from db
+U-  update existing file in db
+D-  delete existing file from db
+*/
