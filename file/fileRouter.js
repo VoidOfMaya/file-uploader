b62d16a7651c8990a312d73355ee9c71723d9b3c
VoidOfMaya <mayaofthevoid@gmail.com>
2026-02-15 15:13:09 +0200

    feat(file validation) updates multer to only allow photos/pdf/doc files

--- file/fileRouter.js
+++ file/fileRouter.js
@@ -6,7 +6,24 @@ import { fileUser } from "../validations/validate.js";
 //const upload = multer({dest: 'uploads/'});
 const storage = multer.memoryStorage();
 
-const upload = multer({storage: storage,limits: { fileSize: 10 * 1022 * 1024}});
+const upload = multer({storage: storage,limits: 
+    { fileSize: 5 * 1022 * 1024},
+    fileFilter: (req, file, cb)=>{
+        const allowedTypes = [
+            "image/jpeg",
+            "image/png",
+            "image/webp",
+            "application/pdf",
+            "application/msword",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        ];
+        if(allowedTypes.includes(file.mimetype)){
+            cb(null,true);
+        }else{
+            cb(new Error("only images, pdgs, and word docs allowed!"));
+        }
+    }
+});
 
 const fileRouter = Router();
 
